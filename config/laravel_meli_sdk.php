7cf342840d4e49ce8838b5ec397a8168487edc9a
Valentin Starck <valentin.starck@gmail.com>
2019-04-16 08:01:03 -0300

    Agregada configuración: site_id

--- config/laravel_meli_sdk.php
+++ config/laravel_meli_sdk.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    'site_id' => env('MELI_SITE_ID', 'MLA'),
     'app_id' => env('MELI_APP_ID'),
     'app_secret' => env('MELI_APP_SECRET'),
     'redirect_url' => env('MELI_REDIRECT_URL', 'meli_auth')
